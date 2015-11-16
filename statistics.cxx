c8bf14f414771a4109f0292b1c30fe97c280513c
theo90 <theo_nikolova@yahoo.com>
2015-11-16 17:29:31 +0100

    Update statistics.cxx

--- statistics.cxx
+++ statistics.cxx
@@ -1,4 +1,4 @@
-#include <cmath>
+/*#include <cmath>
 #include <iostream>
 #include <cstdlib>
 
@@ -18,4 +18,65 @@ int main(){
    cout << "Variance = " << var << endl;
 
    return 0;
+}*/
+#include "stdafx.h"
+#include <iostream>
+#include "stdlib.h"
+#include "conio.h"
+#include "time.h"
+#include <iomanip>
+#include "math.h"
+
+
+using namespace std;
+
+void func_arr(double *p, const int n);
+void func_m_v(double *arrp, double &m, double &v, const int n);
+int main()
+{
+	const int n=3;
+	double arr_x[n];
+	char rep;
+	double m,v;
+
+	cout<<"array start (j/n)";
+	cin>>rep;
+	do{
+		func_arr(arr_x, n);
+		func_m_v(arr_x, m, v, n);
+		cout<<"-------"<<endl;
+		cout<<"m: "<<m<<"v: "<<v;
+		cout<<"\narray noch mal starten (j/n)";
+		cin>>rep;
+	}while(rep=='j');
+	if(rep='n')
+		cout<<"\nfinish";
+    getche();
+    return 0;
 }
+
+void func_arr(double *arrp, const int n)
+{
+	
+    srand((unsigned)time (NULL));              
+	for(int i=0; i<n; i++)
+		arrp[i]=(double) rand()/ RAND_MAX;
+	for(int i=0; i<n; i++)
+		cout<<setprecision(5)<<arrp[i]<<endl;
+}
+void func_m_v(double *arrp, double &m, double &v, const int n)
+{
+	double  sum=0,sum1=0;
+	for(int j=0; j<n; j++)
+	{
+		sum+=arrp[j];
+	}
+	m=sum/n;
+	for(int j=0; j<n; j++)
+	{
+		sum1+=pow((arrp[j]-m),2);
+	}
+	v=sum1/n;
+	//cout<<"m: "<<m<<"v: "<<v<<endl;
+}
+
