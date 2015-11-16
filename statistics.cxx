9702fbd13f9e2ee0698c68881b5cda475010c902
theo90 <theo_nikolova@yahoo.com>
2015-11-16 18:24:26 +0100

    Update statistics.cxx

--- statistics.cxx
+++ statistics.cxx
@@ -1,24 +1,4 @@
-/*#include <cmath>
-#include <iostream>
-#include <cstdlib>
 
-using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
-
-int main(){
-   const int N = 100;
-   double p[N];
-   double mean, var;
-
-   // Some lines here....
-
-   cout << "Mean = " << mean << endl;
-   cout << "Variance = " << var << endl;
-
-   return 0;
-}*/
 #include <iostream>
 #include <cstdlib>
 #include "time.h"
